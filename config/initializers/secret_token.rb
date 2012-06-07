5797fd1e2e1429fd0bf2dfd92e2b118689edbc60
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-06-08 08:33:20 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsReader::Application.config.secret_token = 'e9e317a6325f029a429c8ea27b1fc40ea4fa1538828511c22ae765809c2f7e71fbad1586be8b3a80d118c7ed5472bad70b3ecd8845776b8de2965fbc99534bd5'
